bd0d79368d6b38ce88c0d62f53e118426b9f18f2
Sachira <it21258930@my.sliit.lk>
2025-07-21 20:14:48 +0530

    update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,4 @@
 
 @echo off
-
-start cmd /k "npm i"
 start cmd /k "cd frontend & npm install --save react-credit-card-input styled-components easyinvoice --force & npm i --force"
 start cmd /k "cd backend & npm i"
